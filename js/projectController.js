7c23cc6b761f96e47a1844da1b9526867858e1a5
nitinkathpalia <nn.kathpalia@gmail.com>
2018-05-23 15:20:23 +0530

    Website

--- js/projectController.js
+++ js/projectController.js
@@ -1,24 +1,57 @@
 function show1() {
-    var x = document.getElementById("birdwatcher");
-    var y1 = document.getElementById("mapbot");
+    var x = document.getElementById("buscompanion");
+    var y1 = document.getElementById("blockchain");
     var y2 = document.getElementById("lanechanger");
+	var y3=document.getElementById("SpaceShooter");
+	var y4=document.getElementById("Vehicle");
+	var y5=document.getElementById("Robot");
+	var y6=document.getElementById("Vista");
+	var y7=document.getElementById("Mano");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
     if (x.style.display === "none") {
         x.style.display = "block";
         y1.style.display = "none";
         y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		
     } 
     else {
         x.style.display = "none";
     }
 }
 function show2() {
-    var x = document.getElementById("mapbot");
-    var y1 = document.getElementById("birdwatcher");
+    var x = document.getElementById("blockchain");
+    var y1 = document.getElementById("buscompanion");
     var y2 = document.getElementById("lanechanger");
-    if (x.style.display === "none") {
+	var y3=document.getElementById("SpaceShooter");
+    var y4=document.getElementById("Vehicle");
+	var y5=document.getElementById("Robot");
+	var y6=document.getElementById("Vista");
+	var y7=document.getElementById("Mano");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
         x.style.display = "block";
         y1.style.display = "none";
         y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
     } 
     else {
         x.style.display = "none";
@@ -26,13 +59,270 @@ function show2() {
 }
 function show3() {
     var x = document.getElementById("lanechanger");
-    var y1 = document.getElementById("birdwatcher");
-    var y2 = document.getElementById("mapbot");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("SpaceShooter");
+	var y4=document.getElementById("Vehicle");
+	var y5=document.getElementById("Robot");
+	var y6=document.getElementById("Vista");
+	var y7=document.getElementById("Mano");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+function show4(){
+	var x = document.getElementById("SpaceShooter");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("Vehicle");
+	var y5=document.getElementById("Robot");
+	var y6=document.getElementById("Vista");
+    var y7=document.getElementById("Mano");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y4.style.display="none";
+		y3.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+function show5(){
+	var x = document.getElementById("Vehicle");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Robot");
+	var y6=document.getElementById("Vista");
+	var y7=document.getElementById("Mano");	
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
     if (x.style.display === "none") {
         x.style.display = "block";
         y1.style.display = "none";
         y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
     } 
+    else {
+        x.style.display = "none";
+    }
+}
+function show6(){
+	var x = document.getElementById("Robot");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Vehicle");
+	var y6=document.getElementById("Vista");
+    var y7=document.getElementById("Mano");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		}
+	
+    else {
+        x.style.display = "none";
+    }
+}
+function show7(){
+	var x = document.getElementById("Vista");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Vehicle");
+	var y6=document.getElementById("Robot");
+    var y7=document.getElementById("Mano");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		}
+	
+    else {
+        x.style.display = "none";
+    }
+}
+function show8(){
+	var x = document.getElementById("Mano");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Vehicle");
+	var y6=document.getElementById("Robot");
+    var y7=document.getElementById("Vista");
+	var y8=document.getElementById("GymManager");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		}
+	
+    else {
+        x.style.display = "none";
+    }
+}
+function show9(){
+	var x = document.getElementById("GymManager");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Vehicle");
+	var y6=document.getElementById("Robot");
+    var y7=document.getElementById("Vista");
+	var y8=document.getElementById("Mano");
+	var y9=document.getElementById("HotelSystem");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		}
+		
+	
+    else {
+        x.style.display = "none";
+    }
+}
+function show10(){
+	var x = document.getElementById("HotelSystem");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Vehicle");
+	var y6=document.getElementById("Robot");
+    var y7=document.getElementById("Vista");
+	var y8=document.getElementById("Mano");
+	var y9=document.getElementById("GymManager");
+	var y10=document.getElementById("Crypto");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		}
+		
+	
+    else {
+        x.style.display = "none";
+    }
+}
+function show11(){
+	var x = document.getElementById("Crypto");
+    var y1 = document.getElementById("buscompanion");
+    var y2 = document.getElementById("blockchain");
+	var y3=document.getElementById("lanechanger");
+	var y4=document.getElementById("SpaceShooter");
+	var y5=document.getElementById("Vehicle");
+	var y6=document.getElementById("Robot");
+    var y7=document.getElementById("Vista");
+	var y8=document.getElementById("Mano");
+	var y9=document.getElementById("GymManager");
+	var y10=document.getElementById("HotelSystem");
+	if (x.style.display === "none") {
+        x.style.display = "block";
+        y1.style.display = "none";
+        y2.style.display = "none";
+		y3.style.display="none";
+		y4.style.display="none";
+		y5.style.display="none";
+		y6.style.display="none";
+		y7.style.display="none";
+		y8.style.display="none";
+		y9.style.display="none";
+		y10.style.display="none";
+		}
+		
+	
     else {
         x.style.display = "none";
     }
